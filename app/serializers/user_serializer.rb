b04e3616123fc12dcca140c1447a07c3c0eb0101
github username <github email address>
2018-11-26 18:02:19 -0500

    rails stuff

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,16 +1,16 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :first, :last, :username, :visit_count, :image_src, :restaurants_ranked
   has_many :user_restaurant_rankings
-  has_many :visits, through: :user_restaurant_rankings
-  has_many :restaurants, through: :user_restaurant_rankings
+  # has_many :visits, through: :user_restaurant_rankings
+  # has_many :restaurants, through: :user_restaurant_rankings
 
   class UserRestaurantRankingSerializer < ActiveModel::Serializer
     attributes :id, :restaurant_id, :user_id, :ranking
     has_many :visits
+    belongs_to :restaurant
 
     class RestaurantSerializer < ActiveModel::Serializer
       attributes :id, :name, :address, :cuisine, :location_lat, :location_long, :visitors, :image_src
-      has_many :user_restaurant_rankings
     end
   end
 end
